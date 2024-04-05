d67b7f2415f1da99a67532e64a9872d65393d80e
César <cesarlpb@gmail.com>
2024-04-05 13:06:01 +0200

    Archivos de ejemplo .bashrc y history

---
+++ config/bash_history.sample.sh
@@ -0,0 +1,114 @@
+#1712311793
+ls
+#1712311803
+vim ~/.bashrc
+#1712311812
+nano ~/.bashrc
+#1712311855
+cd ~
+#1712311864
+pwd
+#1712311898
+cd /
+#1712311900
+ls
+#1712311909
+cd ..
+#1712311912
+pwd
+#1712311916
+cd / 
+#1712311917
+pwd
+#1712311931
+cd ~
+#1712311932
+pwd
+#1712311935
+cd / 
+#1712311939
+pwd
+#1712311961
+cd -
+#1712311989
+history
+#1712312118
+nano ~/.bashrc
+#1712312299
+history
+#1712312316
+source ~/.bashrc
+#1712312328
+history
+#1712312337
+nano ~/.bashrc
+#1712312378
+history
+#1712312381
+source ~/.bashrc
+#1712312383
+history
+#1712312440
+nano ~/.bashrc
+#1712312484
+ll
+#1712312493
+ls
+#1712312503
+ls -alF
+#1712312512
+ls -al
+#1712312528
+nano ~/.bashrc
+#1712312617
+source ~/.bashrc
+#1712312622
+cd /
+#1712312628
+cd
+#1712312646
+nano ~/.bashrc
+#1712312730
+source ~/.bashrc
+#1712312738
+cd /
+#1712312742
+cd
+#1712312746
+cd /home
+#1712312776
+nano ~/.bashrc
+#1712312849
+source ~/.bashrc
+#1712312853
+nano ~/.bashrc
+#1712312883
+ls
+#1712312886
+mkdir test
+#1712312888
+cd test
+#1712312891
+ls
+#1712312894
+cd mario
+#1712312897
+ls
+#1712312900
+mkdir test
+#1712312901
+cd test
+#1712312903
+pwd
+#1712312906
+git 
+#1712312917
+sudo apt install git
+#1712312933
+sudo apt update
+#1712313146
+sudo apt install git
+#1712313203
+ls
+#1712313206
+sudo apt install git
